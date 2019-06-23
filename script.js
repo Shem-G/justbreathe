af7315e7fbeff34f7236a8a0ea5c2a9ccb941889
Shem <sgyll@protonmail.com>
2019-06-23 23:57:03 +0300

    Updated menu for mobile.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function showShallow(){
     hideAll()
     void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-shallow")
+    document.getElementById("show-text").innerHTML = 'Shallow <i class="icon icon-caret color-primary"></i>'
     document.getElementById("shallow-button").classList.remove("text-gray")
     
     console.log("Showing Shallow")
@@ -11,6 +12,7 @@ function showMid(){
     hideAll()
     void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-mid")
+    document.getElementById("show-text").innerHTML = 'Mid <i class="icon icon-caret color-primary"></i>'
     document.getElementById("mid-button").classList.remove("text-gray")
     
     console.log("Showing Mid")
@@ -20,6 +22,7 @@ function showDeep(){
     hideAll()
     void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-deep")
+    document.getElementById("show-text").innerHTML = 'Deep <i class="icon icon-caret color-primary"></i>'
     document.getElementById("deep-button").classList.remove("text-gray")
 
     console.log("Showing Deep")
@@ -41,6 +44,7 @@ function hideDeep(){
 }
 
 function hideAll(){
+    document.getElementsByClassName("dot")[0].style.backgroundColor = "#000000AA"
     document.getElementsByClassName("dot")[0].classList.remove("dot-simple-shallow")
     document.getElementsByClassName("dot")[0].classList.remove("dot-simple-mid")
     document.getElementsByClassName("dot")[0].classList.remove("dot-simple-deep")
