0a3cc7a131a697a1169787a3a13672241c023a0a
Shem <sgyll@protonmail.com>
2019-06-23 19:16:22 +0300

    Fixed menus and animation reset.

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 function showShallow(){
-    hideMid()
-    hideDeep()
+    hideAll()
+    void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-shallow")
-    
-
     document.getElementById("shallow-button").classList.remove("text-gray")
+    
     console.log("Showing Shallow")
 }
 
 function showMid(){
+    hideAll()
+    void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-mid")
-    hideShallow()
-    hideDeep()
-
     document.getElementById("mid-button").classList.remove("text-gray")
     
     console.log("Showing Mid")
 }
 
 function showDeep(){
+    hideAll()
+    void document.getElementsByClassName("dot")[0].offsetWidth;
     document.getElementsByClassName("dot")[0].classList.add("dot-simple-deep")
-    hideShallow()
-    hideMid()
-
     document.getElementById("deep-button").classList.remove("text-gray")
+
     console.log("Showing Deep")
 }
 
@@ -40,4 +38,13 @@ function hideMid(){
 function hideDeep(){
         document.getElementsByClassName("dot")[0].classList.remove("dot-simple-deep")
         document.getElementById("deep-button").classList.add("text-gray")
+}
+
+function hideAll(){
+    document.getElementsByClassName("dot")[0].classList.remove("dot-simple-shallow")
+    document.getElementsByClassName("dot")[0].classList.remove("dot-simple-mid")
+    document.getElementsByClassName("dot")[0].classList.remove("dot-simple-deep")
+    document.getElementById("shallow-button").classList.add("text-gray")
+    document.getElementById("mid-button").classList.add("text-gray")
+    document.getElementById("deep-button").classList.add("text-gray")
 }
